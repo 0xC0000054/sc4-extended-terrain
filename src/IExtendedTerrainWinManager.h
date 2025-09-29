e5bf2ee4e3bcbe28e384fa06e0720a40d609ef01
Nicholas Hayes <0xC0000054@users.noreply.github.com>
2025-09-29 15:14:35 -0600

    Re-license the plugin to LGPL 3.0
    This is required by gzcom-dll being licensed as LGPL 2.1 or later.

--- src/IExtendedTerrainWinManager.h
+++ src/IExtendedTerrainWinManager.h
@@ -1,14 +1,23 @@
-////////////////////////////////////////////////////////////////////////////
-//
-// This file is part of sc4-extended-terrain, a DLL Plugin for SimCity 4
-// that adds support for more terrain types.
-//
-// Copyright (c) 2024, 2025 Nicholas Hayes
-//
-// This file is licensed under terms of the MIT License.
-// See LICENSE.txt for more information.
-//
-////////////////////////////////////////////////////////////////////////////
+/*
+ * This file is part of sc4-extended-terrain, a DLL Plugin for SimCity 4
+ * that adds support for more terrain types.
+ *
+ * Copyright (C) 2024, 2025 Nicholas Hayes
+ *
+ * sc4-extended-terrain is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ *
+ * sc4-extended-terrain is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with sc4-extended-terrain.
+ * If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
